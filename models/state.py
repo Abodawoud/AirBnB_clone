bfe703427f48dce1d1e29213bdffcd0cad3e8221
abeermosaad <abeermosaad69@gmail.com>
2023-08-11 20:20:54 +0300

    State class

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module to get the BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State inherits from BaseModel"""
+
+    name = ""
