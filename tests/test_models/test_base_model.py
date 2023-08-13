bdf7271cc8b7397f2e7e6634d5184486cb988dae
Abodawoud <yousefabodawoud@gmail.com>
2023-08-13 09:40:05 +0300

    try check

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,19 +17,20 @@ class TestBaseModel(unittest.TestCase):
         my_model = BaseModel()
         my_model.name = "My First Model"
         my_model.my_number = 89
-        self.assertEqual(my_model.name, "My First Model")
-        self.assertEqual(my_model.my_number, 89)
-        self.assertEqual(my_model.my_number, 89)
         self.assertIsInstance(my_model.id, str)
         self.assertIsInstance(my_model.updated_at, datetime)
         self.assertIsInstance(my_model.created_at, datetime)
         self.assertIsInstance(my_model.name, str)
-        self.assertIsInstance(my_model.__class__.__name__, str)
-        self.assertEqual(my_model.__class__.__name__, 'BaseModel')
         self.assertIsInstance(my_model.my_number, int)
+        self.assertIsInstance(my_model.__class__.__name__, str)
+        self.assertIsInstance(my_model.my_number, int)
+
         pr_my_model = f"[{my_model.__class__.__name__}] \
 ({my_model.id}) {my_model.__dict__}"
         self.assertEqual(str(my_model), pr_my_model)
+        self.assertEqual(my_model.name, "My First Model")
+        self.assertEqual(my_model.my_number, 89)
+        self.assertEqual(my_model.__class__.__name__, 'BaseModel')
 
         my_model_json = my_model.to_dict()
 
